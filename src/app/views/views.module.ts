9be6b54cbab95c33f8fae7a939f22fa053083a2b
Daniel Fischer <DFischer85@comcast.net>
2021-09-07 05:59:57 -0400

    Completed Views Page

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ViewsRoutingModule } from './views-routing.module';
 import { ViewsHomeComponent } from './views-home/views-home.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { SharedModule } from '../shared/shared.module';
+import { ListItemComponent } from './list-item/list-item.component';
 
 
 @NgModule({
   declarations: [
-    ViewsHomeComponent
+    ViewsHomeComponent,
+    StatisticsComponent,
+    ListItemComponent
   ],
   imports: [
     CommonModule,
-    ViewsRoutingModule
+    ViewsRoutingModule,
+    SharedModule
   ],
   exports: []
 })
